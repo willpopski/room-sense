f97ada6def69079ac1b5d62ac02ee337eb492430
willpopski <matthew@nixable.com>
2016-12-02 15:25:29 -0600

    Commented and cleaned up code

--- RoomSenseNode/RoomSenseNode.ino
+++ RoomSenseNode/RoomSenseNode.ino
@@ -1,142 +1,151 @@
+// DHT Sensor library for the DHT22 temperature and humidity sensor
 #include <DHT.h>
+
+// Networking libraries for the data-link layer (Wi-Fi) and transport and session layers (UDP)
 #include "ESP8266WiFi.h"
-
-//#include <WiFiManager.h>
-//#include <DNSServer.h>
-//#include <ESP8266WebServer.h>
-
-#include <WiFiUdp.h>
-#include <math.h>
-
 #include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+
+// Arduino Over The Air allow you to send code updates over WiFi
+// NOTE: If your code update breaks WiFi functionality, then your node will go dark and you will have to physically access the node
 #include <ArduinoOTA.h>
 
-#include <SPI.h>
 
-//for LED status
-#include <Ticker.h>
-Ticker ticker;
+// Update the following section to match your installation
+// ========================================================
 
-//TODO: Move all of these to wifi-configurable parameters
-// and save to JSON config in EEPROM
+// Set a unique, friendly Node name
+String nodeName = "LivingRoom";
+
+// Hostname and UDP port number for the server running influxDB. Can be local or remote, IP Address or DNS name.
 const char* influxHost = "black-pearl";
 const int influxPort = 8086;
-String location = "Room3";
-String measurement = "RoomSense";
-const char* ssid = "SunloftAutomata";
-const char* password = "laserpointer104";
-const char* host = "Room3";
 
-// DHT Sensor Object Here
-DHT dht(D4, DHT22);
+// This is your 
+const char* ssid = "RoomSenseWiFi";
+const char* password = "********";
 
-// Data logging - all time in Milliseconds
-int lastDataLog = 0;
-const int dataLogInterval = 10000;
+// TODO: Move all of these to wifi-configurable parameters and save to JSON config in EEPROM
+// =======================================================
+
+// Setup Sensor Pin and Library object
+// NOTE: The Wemos DHT22 Shield is hardwired to pin Digital 4. Confirm this setting if you are not using the shield.
+const int DHTSensorPin = D4;
+DHT dht(DHTSensorPin, DHT22);
+
+// Data logging values - all time in Milliseconds
+const int sendInterval = 10000;
+int lastSendTimeMillis = 0;
 
 void setup() {
-  // initialize serial communications
-  Serial.begin(115000);
-  pinMode(BUILTIN_LED, OUTPUT);
-  // Start ticker with AP mode and try to connect
-  ticker.attach(0.6, tick);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password); 
+  setupHardware();
+  connectWiFi();
 
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-  
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
-  //WiFiManager wifiManager;
-  //reset settings - for testing
-  //wifiManager.resetSettings();
-
-  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
-  //wifiManager.setAPCallback(configModeCallback);
-
-  //fetches ssid and pass and tries to connect
-  //if it does not connect it starts an access point with the specified name
-  //here  "AutoConnectAP"
-  //and goes into a blocking loop awaiting configuration
-  //if (!wifiManager.autoConnect(ssid,password)) {
-  //  Serial.println("failed to connect and hit timeout");
-  //  //reset and try again, or maybe put it to deep sleep
-  //  ESP.reset();
-  //  delay(1000);
-  //}
-
-  //if you get here you have connected to the WiFi
-  Serial.println("connected... woot! :)");
-  ticker.detach();
+  // Solid LED means we're connected
   digitalWrite(BUILTIN_LED, HIGH);
-
-  dht.begin();
 
   OTASetup();
   
 }
 
 void loop() {
+  // Allow the ArduinoOTA library to periodically monitor and handle firmware updates
   ArduinoOTA.handle();
-  double f = 0;
+
+  // During each loop, the node reads DHT sensor temperature. Sometimes the reading is bad and these readings are skipped
+  // Variable holding temperature reading, accumulator for average, desired number of readings, and a counter of readings
+  double temperatureReading = 0;
+  double averageReading = 0;
   double total = 0;
-  double numberReadings = 10;
+  double desiredReadings = 10;
+  int validReadingsCounter = 0;
 
-  String line;
-
-  int i = 0;
-
-  while (i<numberReadings) {
+  // Loop until we have desired number of readings
+  while (validReadingsCounter < desiredReadings) {
     // get only good readings
-    f = dht.readTemperature(true);
-    if (!isnan(f)) {
-      i++;
-      total += f;
+    temperatureReading = dht.readTemperature(true);
+    if (!isnan(temperatureReading)) {
+      validReadingsCounter++;
+      total += temperatureReading;
     } else {
       Serial.println("Info: Reading nan from sensor");
-      Serial.print("Reading is: ");
-      Serial.println(f);
-      delay(100);
     }
+    // Wait for sensor reading to steady
+    delay(100);
   }
 
-  f = total / numberReadings;
-  
-  if (isnan(f)) {
-    // Don't send temperature
-    Serial.println("Something wrong with sensor!");
-  } else {
-    if ((millis()-lastDataLog) > dataLogInterval) {
-      line = String(measurement + ",type=temperature,unit=degreesFarenheit,location=" + location + " value=" + String(f));
-      WiFiUDP udpClient;
-      if (!!!udpClient.beginPacket(influxHost, influxPort)) {
-        Serial.println("Connection failed, retrying..."); 
-      } else {
-        Serial.print("Connected to ");
-        Serial.println(influxHost);
-        lastDataLog = millis();
-        udpClient.print(line);
-        udpClient.endPacket();
-        blink();
-      }
+  // Calculate average reading
+  averageReading = total / validReadingsCounter;
+
+
+  if ((millis()-lastSendTimeMillis) > sendInterval) {
+    String line = String("RoomSense,type=temperature,unit=degreesFarenheit,location=" + nodeName + " value=" + String(averageReading));
+
+    // Here is our udpClient which needs a host, port number, and payload before sending
+    WiFiUDP udpClient;
+
+    // This attempts to connect and sends data to InfluxDB server
+    if (!udpClient.beginPacket(influxHost, influxPort)) {
+      Serial.println("Connection failed"); 
+    } else {
+      Serial.print("Connected to: ");
+      Serial.println(influxHost);
+      lastSendTimeMillis = millis();
+
+      // Write packet payload
+      udpClient.print(line);
+
+      // Finish off packet and send
+      udpClient.endPacket();
+      blink();
     }  
   }
 }
 
+void setupHardware() {
+  // Initialize serial communications
+  Serial.begin(115000);
+
+  // Configure the pins we will be using
+  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(DHTSensorPin, INPUT);
+
+  // Connect to DHT sensor
+  dht.begin();
+
+}
+
+void connectWiFi() {
+  // BUILTIN_LED is a macro for the builtin LED pin. In Wemos this is D3, but using BUILTIN_LED provides code portability
+  int ledState = digitalRead(BUILTIN_LED);
+
+  // Set ESP8266 mode to act as a Wi-Fi client and attempt to connect with credentials
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  // Fast LED blink while waiting to connect
+  // NOTE: This approach doesn't have have a retry limit and could drain battery if unattended
+   while (WiFi.status() != WL_CONNECTED) {
+     digitalWrite(BUILTIN_LED, ledState);
+     ledState = !ledState;
+     delay(300);
+   }
+
+  //If you get here, then you have connected to the WiFi
+  Serial.println("connected... woot! :)");
+}
+
+
 void OTASetup() {
-    // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
+  // Port defaults to 8266
+  ArduinoOTA.setPort(8266);
 
   // Hostname defaults to esp8266-[ChipID]
-  ArduinoOTA.setHostname(host);
+  ArduinoOTA.setHostname(nodeName.c_str());
 
-  // No authentication by default
-  ArduinoOTA.setPassword((const char *)"grill");
+  // Basic authentication
+  ArduinoOTA.setPassword((const char *)"password");
 
   ArduinoOTA.onStart([]() {
     Serial.println("Start");
